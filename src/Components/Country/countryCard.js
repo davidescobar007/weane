57c6a7c80c26bb353486038ac6f7930163bb940f
DavidEscobar007 <jescobar573@misena.edu.co>
2019-09-15 21:05:22 -0500

    moved currency setState in the redux store to the home.js component. At countryCard component getting just the info from the store

--- src/Components/Country/countryCard.js
+++ src/Components/Country/countryCard.js
@@ -1,63 +1,13 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addCountryInfoAction } from '../../actions/countryActions';
-import { convertedValueAction } from '../../actions/currencyActions';
-import axios from 'axios';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 function CurrencyCard() {
-    const countryCode = useSelector(state => state.weather.weather[0] === undefined ? state.weather.weather[0] : state.weather.weather[0].country);
+
     const location = useSelector(state => state.location.location[0].location === undefined ? state.location.location[0] : state.location.location[0].location);
+    const convertedValue = useSelector(state => state.currency.convertedCurrency[0] === undefined ? "" : state.currency.convertedCurrency[0].convertedValue);
     const currencyCode = useSelector(state => state.country.country[0] === undefined ? state.country.country : state.country.country[0].currency);
-    const convertedValue = useSelector(state => state.currency.convertedCurrency[0] === undefined? state.currency : 2);
-    console.log(currencyCode);
-    const icon = useSelector(state => state.weather.weather[0] === undefined ? state.weather.weather[0] : state.weather.weather[0].icon);
+    // console.log(convertedValue);
 
-    useEffect(() => {
-        getCountryInfo();
-        // getCurrencyConverter();
-    },[])
-
-    const dispatch = useDispatch();
-    const addCountryInfo = (countryInfo) => dispatch(addCountryInfoAction(countryInfo));
-    const addConvertedCurrency = (convertedCurrency) => dispatch(convertedValueAction(convertedCurrency));
-
-    const getCountryInfo = async () => {
-        const urlCountry = `https://restcountries.eu/rest/v2/alpha/${countryCode}`;
-        let info = await axios.get(urlCountry);
-        console.log(info.data.currencies[0].code);
-        addCountryInfo({
-            country: info.data.name,
-            currency: info.data.currencies[0].code,
-            currencySimbol: info.data.currencies[0].symbol,
-            flag: info.data.flag,
-        });
-        const currencyApiKey = "f017f25d741fe49e1536"; //jorgatsu@yopmawil.com 
-        const currencyApiKey1 = "56b0d0350418bb3975fe"; //jorgatsu1@yopmawil.com         
-        const currencyApiKey2 = "68cea0e6d043806969b7"; //jorgatsu2@yopmawil.com         
-        const currencyApiKey3 = "8bc4937eceb0d16c950d"; //jorgatsu3@yopmawil.com         
-        const currencyApiKey4 = "35d0626d49ce6d46bbe9"; //jorgatsu4@yopmawil.com         
-        const currencyApiKey5 = "de6bd5f52e288ad885f8"; //jorgatsu5@yopmawil.com 
-        const urlCurrency = `https://free.currconv.com/api/v7/convert?q=USD_${info.data.currencies[0].code}&compact=ultra&apiKey=${currencyApiKey}`;
-        let currency = await axios.get(urlCurrency);
-        console.log(currency.data);
-        addConvertedCurrency({
-            convertedValue: currency.data
-        });
-    }
-
-
-    const getCurrencyConverter = async () => {
-        const currencyApiKey = "f017f25d741fe49e1536"; //jorgatsu@yopmawil.com 
-        const currencyApiKey1 = "56b0d0350418bb3975fe"; //jorgatsu1@yopmawil.com         
-        const currencyApiKey2 = "68cea0e6d043806969b7"; //jorgatsu2@yopmawil.com         
-        const currencyApiKey3 = "8bc4937eceb0d16c950d"; //jorgatsu3@yopmawil.com 
-        const url = `https://free.currconv.com/api/v7/convert?q=USD_${currencyCode}&compact=ultra&apiKey=${currencyApiKey3}`;
-        let currency = await axios.get(url);
-        // console.log(currency.data);
-        addConvertedCurrency({
-            convertedValue: currency.data
-        })
-    }
 
     return (
         <article background="`http://openweathermap.org/img/wn/$col@2x.png`">
@@ -65,8 +15,7 @@ function CurrencyCard() {
                 <h2>Currency used in {location}</h2>
             </header>
             <div>
-                <h3>$ 1 USD = $  {currencyCode}</h3>
-                <h4>+ 0.25% ↑</h4>
+                <h3>$ 1 USD = $  {convertedValue} {currencyCode}</h3>
             </div>
         </article>
     );
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { addCountryInfoAction } from '../../actions/countryActions';
+import { convertedValueAction } from '../../actions/currencyActions';
+import axios from 'axios';
 import './Home.css';
 import Weather from '../Weather/weatherCard';
 import CountryCard from '../Country/countryCard';
 
+
 function Home() {
+    const countryCode = useSelector(state => state.weather.weather[0] === undefined ? state.weather.weather[0] : state.weather.weather[0].country);
+    
+    const icon = useSelector(state => state.weather.weather[0] === undefined ? state.weather.weather[0] : state.weather.weather[0].icon);
+
+    useEffect(() => {
+        getCountryInfo();
+        //console.log("hey")
+    })
+
+    const dispatch = useDispatch();
+    const addCountryInfo = (countryInfo) => dispatch(addCountryInfoAction(countryInfo));
+    const addConvertedCurrency = (convertedCurrency) => dispatch(convertedValueAction(convertedCurrency));
+
+    const getCountryInfo = async () => {
+        if (countryCode !== undefined) {
+            const urlCountry = `https://restcountries.eu/rest/v2/alpha/${countryCode}`;
+            let info = await axios.get(urlCountry);
+            // console.log(info.data.currencies[0].code);
+            addCountryInfo({
+                country: info.data.name,
+                currency: info.data.currencies[0].code,
+                currencySimbol: info.data.currencies[0].symbol,
+                flag: info.data.flag,
+            });
+            const currencyApiKey = "f017f25d741fe49e1536"; //jorgatsu@yopmawil.com 
+            const currencyApiKey1 = "56b0d0350418bb3975fe"; //jorgatsu1@yopmawil.com         
+            const currencyApiKey2 = "68cea0e6d043806969b7"; //jorgatsu2@yopmawil.com         
+            const currencyApiKey3 = "8bc4937eceb0d16c950d"; //jorgatsu3@yopmawil.com         
+            const currencyApiKey4 = "35d0626d49ce6d46bbe9"; //jorgatsu4@yopmawil.com         
+            const currencyApiKey5 = "de6bd5f52e288ad885f8"; //jorgatsu5@yopmawil.com 
+            
+                const urlCurrency = `https://free.currconv.com/api/v7/convert?q=USD_${info.data.currencies[0].code}&compact=ultra&apiKey=${currencyApiKey1}`;
+                let currency = await fetch(urlCurrency);
+                let currencyResult = await currency.json();
+                const currencyResultToArray = Object.entries(currencyResult);
+                console.log(currencyResultToArray[0][1]);
+                addConvertedCurrency({
+                    convertedValue: currencyResultToArray[0][1]
+                });
+        }
+
+      
+        
+    }
 
     return (
         <div>
@@ -13,16 +62,16 @@ function Home() {
                     <a href="#!" className="home_card child">
                         <article>
                             <header>
-                                <h2>Bucaramanga</h2>
+                                <h2>We are so sorry :(</h2>
                             </header>
                             <div>
-                                <h4>29°c</h4>
-                                <h4>Sunny</h4>
+                                <h4>This feature will be available soon</h4>
+                                <h4></h4>
                             </div>
                         </article>
                     </a>
                     <a href="#!" className="home_card child">
-                       <CountryCard></CountryCard>
+                        <CountryCard></CountryCard>
                     </a>
                 </div>
             </section>
