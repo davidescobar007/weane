f73e26102a96e0c5a66c6787ab61abbb0e232cc6
david <jescobar573@misena.edu.co>
2019-09-16 22:14:58 -0500

    currency simbol updated, changing reagrding currency used in country component

--- src/Components/Country/countryCard.js
+++ src/Components/Country/countryCard.js
@@ -6,6 +6,7 @@ function CurrencyCard() {
     const location = useSelector(state => state.location.location[0].location === undefined ? state.location.location[0] : state.location.location[0].location);
     const convertedValue = useSelector(state => state.currency.convertedCurrency[0] === undefined ? "" : state.currency.convertedCurrency[0].convertedValue);
     const currencyCode = useSelector(state => state.country.country[0] === undefined ? state.country.country : state.country.country[0].currency);
+    const currencySimbol = useSelector(state => state.country.country[0] === undefined ? state.country.country : state.country.country[0].currencySimbol);
     // console.log(convertedValue);
 
 
@@ -15,7 +16,7 @@ function CurrencyCard() {
                 <h2>Currency used in {location}</h2>
             </header>
             <div>
-                <h3>$ 1 USD = $  {convertedValue} {currencyCode}</h3>
+                <h3>$ 1 USD = {currencySimbol}  {convertedValue} {currencyCode}</h3>
             </div>
         </article>
     );
